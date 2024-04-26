f6d8d6a8d85db90192f293989eec206d637c045e
Phillip Mejia <106935426+oscarmejia1776@users.noreply.github.com>
2024-04-26 13:21:42 -0400

    Add files via upload

---
+++ Lab12Prob03.java
@@ -0,0 +1,29 @@
+//package lab12;
+
+/* 
+* Name : Phillip Mejia, Alejandro Giron
+* File: Lab12Prob03.java
+* Course : CSCI 1302
+* Description : Recursive Pointer function
+* Last Modified : April 26, 2024
+*/
+
+public class Lab12Prob03 {
+	public static String reverseString(String word) {
+		if (word.length() == 1) { // Base Case
+			return word;
+		} else {
+			// Recursive call to the function
+			return word.substring(word.length() - 1) + reverseString(word.substring(0, word.length() - 1));
+		}
+	}
+	
+	public static String reverseStringHelper(String word, int num) {
+		if (num == 0) { // Base Case
+			return String.valueOf(word.charAt(0));
+		} else {
+			// Recursive call to the function
+			return word.charAt(num) + reverseStringHelper(word, num - 1);
+		}
+	}
+}
